c2d4d74d8d80bd94825f01c176ec26b6dc6fd37c
Flamenser <elshavoloco23@gmail.com>
2025-02-15 20:16:50 -0800

    Picos aleatorios

--- Juego.gd
+++ Juego.gd
@@ -1,13 +1,40 @@
 extends Node2D
-var score = 0 # Puntuación del jugador
+
+var score = 0
+var picos_izquierda = []
+var picos_derecha = []
 
 func _ready():
-	update_score()  # Actualizar la puntuación al inicio
+	update_score()
+	obtener_picos()
+	debug_nodos()
+func obtener_picos():
+	# Llenar las listas con los picos según su nombre
+	for i in range(1, 20):  # Picos de la izquierda (1 al 19)
+		picos_izquierda.append(get_node("Pico" + str(i)))
+	for i in range(20, 41):  # Picos de la derecha (20 al 40)
+		picos_derecha.append(get_node("Pico" + str(i)))
+func debug_nodos():
+	for nodo in get_tree().get_root().get_children():
+		print("📌 Nodo encontrado en root: " + nodo.name)
+
 func add_score():
-	score += 1  # Aumentar puntuación
-	update_score()  # Reflejar el cambio en pantalla
+	score += 1
+	update_score()
+	activar_pico_random()
+
 func update_score():
-	$ScoreLabel.text = str(score)  # Actualizar el texto en pantalla
+	$ScoreLabel.text = "Puntos: " + str(score)
+
+func activar_pico_random():
+	if $Jugador.position.x <= 0 or position.x >= 643:
+		 # Si el jugador está en la izquierda, activamos un pico de la derecha
+		var pico = picos_derecha[randi() % picos_derecha.size()]
+		pico.show()
+	else:
+		# Si el jugador está en la derecha, activamos un pico de la izquierda
+		var pico = picos_izquierda[randi() % picos_izquierda.size()]
+		pico.show()
 
 func _on_reinicio_pressed():
 	get_tree().paused = false
@@ -24,9 +51,4 @@ func on_game_over():
 	
 func _on_death_zone_body_entered(body):
 	if body.name == "Jugador":  # Asegúrate de que es el jugador
-		on_game_over()
-
-
-func _on_pico_body_entered(body):
-	if body.name == "Jugador" and $Pico.show:  # Asegúrate de que es el jugador
 		on_game_over()
